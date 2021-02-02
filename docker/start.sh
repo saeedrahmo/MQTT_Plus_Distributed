091eac5974e1ec2b6f9d13a410f233deb1c542be
Ubuntu <edoardesd@gmail.com>
2021-02-02 20:37:40 +0000

    add logger

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,7 @@
 
 echo "Executing the JAR ..."
 
-java -jar /home/mqttplus/MQTT_Plus_Distributed/out/artifacts/MQTTPlusWS_jar/MQTTPlusWS.jar "${WS_PORT}" "${BROKER_PORT}" "${DISTRIBUTED_FLAG}"  "${TOPOLOGY}" "${CLUSTER_SIZE}" "${LOCAL_FLAG}" &> /home/mqttplus/jar_out.log &
+java -jar /home/mqttplus/MQTT_Plus_Distributed/out/artifacts/MQTTPlusWS_jar/MQTTPlusWS.jar "${WS_PORT}" "${BROKER_PORT}" "${DISTRIBUTED_FLAG}"  "${TOPOLOGY}" "${CLUSTER_SIZE}" "${LOCAL_FLAG}" |& tee /home/mqttplus/jar_out.log &
 
 sleep 3
 
